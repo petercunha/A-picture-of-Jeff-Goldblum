4eac79584126ab03b2d2d82240508284e8b66b5f
Brian ITCOB Smith <briantcob@gmail.com>
2017-10-09 15:24:59 +0200

    Update to Secure Hash Algorithm to avoid MD5 Jeff Collisions
    While MD5 may provide some protections in securing Jeff, please consider how an attacker could use existing vectors to alter the original Jeff and still produce an identical MD5 checksum. Please refer to the following paper by Anton A. Kuznetsov demonstrating how it is possible to cause single-block collisions using high performance computing clusters https://eprint.iacr.org/2014/871.pdf

--- hash.rb
+++ hash.rb
@@ -1,9 +1,7 @@
-require 'digest/md5'
+require 'digest/sha2'
 
-jeff = 'abd7f453bd7c4bfee0824cf1546b6cd8'
 f = './jeff.jpg'
-
-digest = Digest::MD5.hexdigest(File.read(f))
+digest = Digest::SHA2.new(512).hexdigest(File.read(f))
 
 if jeff == digest
   puts 'Success, Jeff is validated'
